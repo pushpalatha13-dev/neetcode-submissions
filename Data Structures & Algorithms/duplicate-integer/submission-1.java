44f83c0b25a621f6669540789a54197858520409
pushpalatha13-dev <gpushpalatha191@gmail.com>
2026-07-23 04:35:57 +0000

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet <Integer> s=new HashSet<>();
+        for(int n:nums){
+            if(s.contains(n)){
+                return true;
+            }
+            s.add(n);
+        }
+        return false;
+        
+    }
+}
